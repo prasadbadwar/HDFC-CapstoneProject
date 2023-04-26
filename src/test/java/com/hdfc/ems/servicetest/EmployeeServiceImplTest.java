9cfc711f3f3a2fea970e0ce0e45b329b6695b0c6
Prasad Badwar <pbbadwar123@gmail.com>
2023-04-26 12:27:25 +0530

    Unit Testing

--- src/test/java/com/hdfc/ems/servicetest/EmployeeServiceImplTest.java
+++ src/test/java/com/hdfc/ems/servicetest/EmployeeServiceImplTest.java
@@ -2,14 +2,12 @@ package com.hdfc.ems.servicetest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.hdfc.ems.dto.EmployeeDTO;
-import com.hdfc.ems.exception.EmpNotFound;
-import com.hdfc.ems.service.EmployeeServiceImpl;
+import com.hdfc.ems.exception.EmployeeNotFound;
 import com.hdfc.ems.service.IEmployeeService;
 
 @SpringBootTest
@@ -19,7 +17,7 @@ class EmployeeServiceImplTest {
 	@Autowired
 	IEmployeeService service;
 	@Test
-	void testGetEmployee() throws EmpNotFound,Exception {
+	void testGetEmployee() throws EmployeeNotFound,Exception {
 		EmployeeDTO emp=service.getEmployee(101);
 		assertNotNull(emp);
 	}
