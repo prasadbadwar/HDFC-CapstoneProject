e0965ba60ba9d1da1ae3a354fbfabde3935af453
Prasad Badwar <pbbadwar123@gmail.com>
2023-04-26 12:38:37 +0530

    Service Interface

--- src/main/java/com/hdfc/ems/service/IEmployeeService.java
+++ src/main/java/com/hdfc/ems/service/IEmployeeService.java
@@ -6,9 +6,9 @@
 package com.hdfc.ems.service;
 
 import com.hdfc.ems.dto.EmployeeDTO;
-import com.hdfc.ems.exception.EmpNotFound;
+import com.hdfc.ems.exception.EmployeeNotFound;
 
 public interface IEmployeeService {
 
-		public EmployeeDTO getEmployee(long id) throws EmpNotFound,Exception;
+		public EmployeeDTO getEmployee(long id) throws EmployeeNotFound,Exception;
 }
