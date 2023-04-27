c9b8f2a730240e26be8bdbf1a50d3df9f2fd70c7
Prasad Badwar <pbbadwar123@gmail.com>
2023-04-27 12:53:38 +0530

    Unit Testing

--- src/test/java/com/hdfc/ems/servicetest/EmployeeServiceImplTest.java
+++ src/test/java/com/hdfc/ems/servicetest/EmployeeServiceImplTest.java
@@ -1,8 +1,6 @@
 package com.hdfc.ems.servicetest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
