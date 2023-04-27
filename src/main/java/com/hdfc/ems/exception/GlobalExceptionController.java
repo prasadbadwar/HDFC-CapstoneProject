23898e113fbea1fb72e1f40a472e4575986256de
Prasad Badwar <pbbadwar123@gmail.com>
2023-04-27 22:28:31 +0530

    Global Exception Handler

--- src/main/java/com/hdfc/ems/exception/GlobalExceptionController.java
+++ src/main/java/com/hdfc/ems/exception/GlobalExceptionController.java
@@ -9,13 +9,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionController {
 	
 	@ExceptionHandler(EmployeeNotFound.class)
-	public ResponseEntity<String> handleBookExp(EmployeeNotFound e){
+	public ResponseEntity<String> handleEmployeeExp(EmployeeNotFound e){
 		return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<String> handleInputExp(MethodArgumentTypeMismatchException e){
+		return new ResponseEntity<>("Input value should be numbered...",HttpStatus.BAD_REQUEST);
 	}
 	
 }
